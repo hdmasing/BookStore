98f79a96402daf8d900aec5f915817bfcd36647b
H-D Masing <simpson1997@windowslive.com>
2019-12-15 16:44:28 +0200

    Creating bookstore

---
+++ BookStore/BookStore/DatabaseContext/BookStoreDatabaseInitializer.cs
@@ -0,0 +1,49 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.DatabaseContext
+{
+    public class BookStoreDatabaseInitializer : DropCreateDatabaseIfModelChanges<BookStoreDbContext>
+    {
+        protected override void Seed(BookStoreDbContext context)
+        {
+            context.Books.AddOrUpdate(
+                x => x.Name,
+                new Book
+                {
+                    Name = "apple",
+                    Price = 1,
+                    Description = "Hea raamat",
+                    Author = "Jaan Tätte",
+                    Year=1998
+                    
+                },
+                new Book
+                {
+                    Name = "bread",
+                    Price = 0.8
+                },
+                new Book
+                {
+                    Name = "milk",
+                    Price = 1.1
+                },
+                new Book
+                {
+                    Name = "chocolate",
+                    Price = 2
+                },
+                new Book
+                {
+                    Name = "potatoes",
+                    Price = 0.6
+                }
+                );
+        }
+    }
+}
---
+++ BookStore/BookStore/Models/ShoppingCart.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class ShoppingCart
+    {
+        public Guid Id { get; set; }
+
+        public double Sum { get; set; }
+
+        public DateTime DateCreated { get; private set; }
+        public Boolean Paid { get; set; }
+
+        public virtual ICollection<Book> Items { get; set; }
+
+        public ShoppingCart()
+        {
+            Id = Guid.NewGuid();
+            Sum = 0;
+            DateCreated = DateTime.Now;
+            Items = new List<Book>();
+        }
+
+        public void AddToCart(Book book)
+        {
+            Items.Add(book);
+            Sum += book.Price;
+        }
+    }
+}
